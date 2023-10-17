2aa42a7d66a7ccc1a0aa2fc0d3d88f6e89e39508
selvianarsanti <145754961+selvianarsanti@users.noreply.github.com>
2023-10-17 20:57:43 +0700

    tugaspertemuan4

--- v_about.php
+++ v_about.php
@@ -2,18 +2,18 @@
     <h1><?php echo $judul ?></h1>
     <h4>Nama</h4> 
     <ul type="disc"> 
-        <li>Nama Depan : Imam</li> 
-        <li>Nama Belakang : Nawawi</li> 
+        <li>Nama Depan : Selviana Dwi</li> 
+        <li>Nama Belakang : Sari Arsanti</li> 
     </ul> 
     <br> 
     <h4>Alamat</h4> 
     <ul type="none"> 
-        <li> Jalan Ciledug Raya No. 168 Pesanggrahan</li> 
+        <li> Tegalampel,Karangdowo,Klaten</li> 
     </ul> 
     
     <h4>Tempat Lahir</h4> 
     <ul type="none"> 
-        <li>Cirebon</li> 
+        <li>Klaten</li> 
     </ul> 
     
     <h4>Olah Raga Favorit</h4> 
