0bbc98079fdfc6e41f8fb68771df050858de7efc
selvianarsanti <145754961+selvianarsanti@users.noreply.github.com>
2023-11-03 16:10:01 +0700

    Add files via upload

---
+++ Mahasiswa.php
@@ -0,0 +1,25 @@
+<?php 
+class Mahasiswa extends CI_Controller 
+{ 
+    public function index() 
+    { 
+        $this->load->view('view-form-mahasiswa'); 
+
+    } 
+    
+    public function cetak() 
+     {       $data = [ 
+                'nama' => $this->input->post('nama'), 
+                'nim' => $this->input->post('nim'), 
+                'kelas' => $this->input->post('kelas'), 
+                'tgl' => $this->input->post('tgl'), 
+                'tempat' => $this->input->post('tempat'), 
+                'alamat' => $this->input->post('alamat'),
+                'jenis' => $this->input->post('jenis'), 
+                'agama' => $this->input->post('agama')  
+            ]; 
+            
+            $this->load->view('view-data-mahasiswa', $data); 
+    } 
+}
+
